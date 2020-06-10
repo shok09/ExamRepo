a3d3b2803ee3c51d60e1a08365f627b8bba7091d
shok09 <shok09111@gmail.com>
2020-06-10 17:30:15 +0300

    RoomService Imitation

--- BLL/Services/RoomService.cs
+++ BLL/Services/RoomService.cs
@@ -1,10 +1,60 @@
-﻿using System;
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Exceptions;
+using BLL.Services.Base;
+using BLL.Services.Interfaces;
+using DAL.Entities;
+using DAL.Entities.Enums;
+using DAL.UnitOfWork;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BLL.Services
 {
-    class RoomService
+    public class RoomService : BaseService, IRoomService
     {
+        public RoomService(IUnitOfWork unitOfWork, IMapper mapper)
+               : base(unitOfWork, mapper) { }
+        public async Task ChangeToxicityAsync(RoomDTO roomDTO)
+        {
+            var searchedRoom = await _unitOfWork.RoomRepository
+                .GetByIdAsync(roomDTO.Id);
+
+            await _unitOfWork.RoomRepository.UpdateAsync(searchedRoom);
+        }
+        public async Task CreateRoomAsync(RoomDTO roomDTO)
+        {
+            var searchedRoom = await _unitOfWork.RoomRepository
+                .GetByIdAsync(roomDTO.Id);
+
+            await _unitOfWork.RoomRepository.UpdateAsync(searchedRoom);
+        }
+        public async Task DeleteRoomAsync(RoomDTO roomDTO)
+        {
+            var searchedRoom = await _unitOfWork.RoomRepository
+                .GetByIdAsync(roomDTO.Id);
+
+            await _unitOfWork.RoomRepository.UpdateAsync(searchedRoom);
+        }
+        public async Task ClearRoomAsync(RoomDTO roomDTO)
+        {
+
+            var searchedRoom = await _unitOfWork.RoomRepository
+                .GetByIdAsync(roomDTO.Id);
+
+            await _unitOfWork.RoomRepository.UpdateAsync(searchedRoom);
+
+        }
+        public async Task<RoomDTO> GetRoomByIdAsync(int id)
+        {
+            var searchedRoom = await _unitOfWork.RoomRepository
+                .GetByIdAsync(id);
+
+            if (searchedRoom == null)
+                throw new NotFoundException("Room");
+
+            return _mapper.Map<RoomDTO>(searchedRoom);
+        }
     }
 }

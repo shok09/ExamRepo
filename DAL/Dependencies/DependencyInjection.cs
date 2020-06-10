b718834ca70e5f83abfe09a600fe918a6a057fef
shok09 <shok09111@gmail.com>
2020-06-10 16:20:28 +0300

    DependencyInjection

---
+++ DAL/Dependencies/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DAL.EF;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using DAL.UnitOfWork;
+
+namespace DAL.Dependencies
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDalConfiguration
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StorageContext>(options =>
+            options.UseLazyLoadingProxies().
+            UseSqlServer(configuration.
+            GetConnectionString("StorageDataBase")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+            return services;
+        }
+    }
+}

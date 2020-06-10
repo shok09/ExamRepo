4d2364dc982440ee66d283bfa546ba8e2899ffe2
shok09 <shok09111@gmail.com>
2020-06-10 16:45:27 +0300

    Dependency Injection

--- BLL/Dependencies/DependencyInjection.cs
+++ BLL/Dependencies/DependencyInjection.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
+using BLL.Services;
+using BLL.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
 
 namespace BLL.Dependencies
 {
-    class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddBllConfiguration
+            (this IServiceCollection services)
+        {
+            services.AddScoped<IRoomService, RoomService>();
+            services.AddScoped<IGoodsService, GoodsService>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
     }
 }

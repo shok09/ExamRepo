326964175cabafeaab22cfec4dea5d3fd371eb19
shok09 <shok09111@gmail.com>
2020-06-10 15:49:19 +0300

    Added DbContext

---
+++ DAL/EF/StorageContext.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace DAL.EF
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext(DbContextOptions<StorageContext> options)
+            : base(options) { }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Goods> AllGoods { get; set; }
+        public DbSet<DateInfo> DateInfos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DAL/EF/StorageContextSeeder.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.EF
+{
+    public static class StorageContextSeeder
+    {
+        public static void Seed(StorageContext context)
+        {
+            var roomsList = new List<Room>
+            {
+                new Room { TotalVolume = 10000, Toxicity = Entities.Enums.ToxicityLevel.Innocuous},
+                new Room { TotalVolume = 5000, Toxicity = Entities.Enums.ToxicityLevel.Harmful },
+                new Room { TotalVolume = 2000, Toxicity = Entities.Enums.ToxicityLevel.Destructive }
+            };
+            var goodsList = new List<Goods>
+            {
+                new Goods
+                {
+                    Title = "Gala",
+                    Description = "Plate washing tool",
+                    Volume = 100,
+                    Toxicity = Entities.Enums.ToxicityLevel.Innocuous,
+                    ValidityDate = new DateInfo { DateOfMan = new DateTime(2020, 04, 03), DateOfExpire = new DateTime(2024, 10, 03) }
+                },
+                new Goods
+                {
+                    Title = "Cif",
+                    Description = "Toilet washing tool",
+                    Volume = 60,
+                    Toxicity = Entities.Enums.ToxicityLevel.Harmful,
+                    ValidityDate = new DateInfo { DateOfMan = new DateTime(2020, 03, 02), DateOfExpire = new DateTime(2023, 09, 02) }
+                },
+                new Goods
+                {
+                    Title = "Destructor",
+                    Description = "Microbe and dirt destruction tool",
+                    Volume = 30,
+                    Toxicity = Entities.Enums.ToxicityLevel.Destructive,
+                    ValidityDate = new DateInfo { DateOfMan = new DateTime(2014, 02, 23), DateOfExpire = new DateTime(2034, 02, 23) }
+                }
+            };
+            foreach (var goods in goodsList)
+                context.AllGoods.Add(goods);
+            foreach (var room in roomsList)
+                context.Rooms.Add(room);
+
+            context.SaveChanges();
+        }
+    }
+}
